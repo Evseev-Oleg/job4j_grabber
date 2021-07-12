bd54bdaa797ff57218c6dfbcac54ad8428580cfd
Evseev Oleg <oleg063053@gmail.com>
2021-07-12 09:45:29 +0400

    4. Схема таблицы Post [#1731]

--- db/scripts/shemaPost.sql
+++ db/scripts/shemaPost.sql
@@ -2,6 +2,7 @@ CREATE TABLE post(
     id serial primary key,
 	name varchar(255),
     text text,
-    link varchar(255),
-    created date
+    link varchar(255) NOT NULL,
+    created date,
+    CONSTRAINT link_unique UNIQUE (link)
 );

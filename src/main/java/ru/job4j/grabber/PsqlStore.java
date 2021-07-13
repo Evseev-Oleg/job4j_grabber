1be466aff74beac815d7cf73be64f962e0febc59
Evseev Oleg <oleg063053@gmail.com>
2021-07-14 02:15:17 +0400

    5. PsqlStore [#285209]

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,108 @@
+package ru.job4j.grabber;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store, AutoCloseable {
+    private Connection connection;
+
+    public Connection psqlStore(Properties cfg) {
+        try (InputStream in = PsqlStore.class.getClassLoader()
+                .getResourceAsStream("rabbit.properties")) {
+            cfg.load(in);
+            Class.forName(cfg.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    cfg.getProperty("url"),
+                    cfg.getProperty("username"),
+                    cfg.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        return connection;
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement statement = psqlStore(new Properties()).prepareStatement(
+                "INSERT INTO post (name, text, link, created) VALUES (?, ?, ?, ?)",
+                Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getLink());
+            statement.setString(3, post.getDescription());
+            statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            statement.execute();
+            try (ResultSet generatedKey = statement.getGeneratedKeys()) {
+                while (generatedKey.next()) {
+                    post.setId(generatedKey.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> postList = new ArrayList<>();
+        try (PreparedStatement statement = psqlStore(new Properties()).prepareStatement(
+                "select * from post")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    Post post = new Post(
+                            resultSet.getString("name"),
+                            resultSet.getString("text"),
+                            resultSet.getString("link"),
+                            resultSet.getTimestamp("created").toLocalDateTime());
+                    post.setId(resultSet.getInt("id"));
+                    postList.add(post);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return postList;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = null;
+        try (PreparedStatement statement = psqlStore(new Properties()).prepareStatement(
+                "select * from post where id = ?")) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    post = new Post(
+                            resultSet.getString("name"),
+                            resultSet.getString("text"),
+                            resultSet.getString("link"),
+                            resultSet.getTimestamp("created").toLocalDateTime());
+                    post.setId(resultSet.getInt("id"));
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        PsqlStore psqlStore = new PsqlStore();
+        psqlStore.save(new Post("Name", "//-//-//", "http...", LocalDateTime.now()));
+        List<Post> list = psqlStore.getAll();
+        System.out.println(list);
+        Post post = psqlStore.findById(1);
+        System.out.println(post);
+    }
+}
